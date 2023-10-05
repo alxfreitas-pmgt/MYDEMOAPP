c0d1acafecc4d95d82b31ee80269845470820556
alxfreitas-pmgt <alex.freitas@pmgt.com.br>
2023-10-05 01:17:40 -0300

    ajuste de  projeto

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
-# print "O hostane deste aplicativo é  " gethostname() "\n";
-echo gethostname();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hostname do Servidor</title>
+</head>
+<body>
+    <h1>Hostname do Servidor</h1>
+    <?php
+    // Obtém o nome do host do servidor
+    $hostname = gethostname();
+    
+    // Exibe o hostname na página web
+    echo "<p>O hostname do servidor é: $hostname</p>";
+    ?>
+</body>
+</html>
