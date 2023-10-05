ea5fb789cf6641ba0029786e7866d8406e523005
alxfreitas-pmgt <alex.freitas@pmgt.com.br>
2023-10-05 02:35:06 -0300

    adicionado controle de cookie para preserver definição do usuário

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     
     <div>
         <label for="tempoAtualizacao">Tempo de Atualização (segundos):</label>
-        <input type="number" id="tempoAtualizacao" min="1" step="1" value="30">
+        <input type="number" id="tempoAtualizacao" min="1" step="1">
         <button id="salvar" style="background-color: green;">Salvar</button>
         <button id="redefinir" style="background-color: red;">Redefinir</button>
     </div>
@@ -52,26 +52,38 @@
             intervalo = setInterval(atualizacao, 1000);
         }
 
-        // Carregar o contador regressivo com o valor padrão
-        let tempoAtualizacao = parseInt(document.getElementById('tempoAtualizacao').value) || 30;
+        // Carregar o contador regressivo com o valor do cookie, se disponível
+        let tempoAtualizacao = parseInt(getCookie('tempoAtualizacao')) || 30;
+        document.getElementById('tempoAtualizacao').value = tempoAtualizacao;
         atualizarContador(tempoAtualizacao);
 
         // Adicionar evento de clique no botão "Salvar"
         document.getElementById('salvar').addEventListener('click', function () {
             tempoAtualizacao = parseInt(document.getElementById('tempoAtualizacao').value) || 30;
+            setCookie('tempoAtualizacao', tempoAtualizacao, 365); // Armazenar o valor em um cookie
             atualizarContador(tempoAtualizacao);
         });
 
         // Adicionar evento de clique no botão "Redefinir"
         document.getElementById('redefinir').addEventListener('click', function () {
+            tempoAtualizacao = 30; // Valor padrão
             document.getElementById('tempoAtualizacao').value = tempoAtualizacao;
+            setCookie('tempoAtualizacao', tempoAtualizacao, 365); // Armazenar o valor em um cookie
             atualizarContador(tempoAtualizacao);
         });
+
+        // Função para definir um cookie
+        function setCookie(name, value, days) {
+            const expires = new Date();
+            expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+            document.cookie = name + '=' + value + ';expires=' + expires.toUTCString();
+        }
+
+        // Função para obter o valor de um cookie
+        function getCookie(name) {
+            const keyValue = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+            return keyValue ? keyValue[2] : null;
+        }
     </script>
 </body>
 </html>
-
-
-
-
-
