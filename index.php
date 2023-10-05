8559f734a892983c3422d27c18e51933e901902b
alxfreitas-pmgt <alex.freitas@pmgt.com.br>
2023-10-05 01:26:24 -0300

    nova edição do projeto

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hostname do Servidor</title>
+    <title>Hostname e Endereço IP do Servidor</title>
 </head>
 <body>
-    <h1>Hostname do Servidor</h1>
+    <h1>Hostname e Endereço IP do Servidor</h1>
     <?php
     // Obtém o nome do host do servidor
     $hostname = gethostname();
     
-    // Exibe o hostname na página web
+    // Obtém o endereço IP do servidor
+    $server_ip = $_SERVER['SERVER_ADDR'];
+    
+    // Exibe o hostname e o endereço IP na página web
     echo "<p>O hostname do servidor é: $hostname</p>";
+    echo "<p>O endereço IP do servidor é: $server_ip</p>";
     ?>
 </body>
 </html>
