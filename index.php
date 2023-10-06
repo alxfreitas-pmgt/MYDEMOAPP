34a2d9e6552cb57f309b2049b61353868b7d9cbe
alxfreitas-pmgt <alex.freitas@pmgt.com.br>
2023-10-06 01:58:18 -0300

    refatoração

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simulação de Carga</title>
+    <meta http-equiv="refresh" content="10">
+</head>
+<body>
+    <h1>Informações do Servidor:</h1>
+    <?php
+        // Obtém o hostname do servidor hospedeiro
+        $hostname = gethostname();
+        echo "Hostname: " . $hostname . "<br>";
+
+        // Obtém o IP do servidor hospedeiro
+        $ip = gethostbyname($hostname);
+        echo "IP do Servidor: " . $ip . "<br>";
+    ?>
+
+    <h2>Simular Carga:</h2>
+    <form method="post">
+        <input type="submit" name="simular_carga" value="Simular Carga">
+    </form>
+
+    <?php
+        if (isset($_POST['simular_carga'])) {
+            // Simula carga fazendo 20 conexões com o servidor local na porta 8080 usando cURL
+            for ($i = 0; $i < 20; $i++) {
+                $ch = curl_init("http://localhost:8080");
+                curl_exec($ch);
+                curl_close($ch);
+            }
+            echo "Carga simulada com sucesso!";
+        }
+    ?>
+</body>
+</html>
